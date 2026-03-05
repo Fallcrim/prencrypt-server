72db36b2795ef537e7ea4ce6fb7ce4a27c50adde
Fallcrim <80700363+Fallcrim@users.noreply.github.com>
2026-03-05 15:02:31 +0100

    Moved userid conversion to Message

--- utils/message.py
+++ utils/message.py
@@ -3,11 +3,11 @@ from . import convert_userid
 
 class Message:
     OPCODES = {
-        0x00: "UKN",  # docs: unknown
-        0x11: "REG",  # docs: register
-        0x12: "GET",  # docs: get
-        0x13: "TRF",  # docs: transfer
-        0xFF: "ERR",  # docs: error
+        "0x00": "UKN",  # docs: unknown
+        "0x11": "REG",  # docs: register
+        "0x12": "GET",  # docs: get
+        "0x13": "TRF",  # docs: transfer
+        "0xFF": "ERR",  # docs: error
     }
 
     OPCODE_OFFSET = 0
@@ -29,15 +29,19 @@ class Message:
         """
         return f"{self.opcode}{self.userid}{self.signature}{self.data}".encode()
 
-    @staticmethod
-    def parse_message(message: bytes):
+    @classmethod
+    def parse_message(cls, message: bytes) -> 'Message':
         """
         Parses a bytes representation of the message and returns a Message object.
         :param message:
         :return:
         """
-        opcode_b = message[Message.OPCODE_OFFSET]
-        userid_b = message[Message.USERID_OFFSET:Message.USERID_OFFSET + 4]  # userid length = 4
-        signature_b = message[Message.SIGNATURE_OFFSET:Message.SIGNATURE_OFFSET + 256]  # signature length = 256
-        data = message[Message.DATA_OFFSET:]
-        return Message(hex(opcode_b), convert_userid(userid_b), signature_b, data)
+        opcode_b = message[cls.OPCODE_OFFSET]
+        userid_b = message[cls.USERID_OFFSET:cls.USERID_OFFSET + 4]  # userid length = 4
+        signature_b = message[cls.SIGNATURE_OFFSET:cls.SIGNATURE_OFFSET + 256]  # signature length = 256
+        data = message[cls.DATA_OFFSET:]
+        return Message(hex(opcode_b), cls.convert_userid(userid_b), signature_b, data)
+
+    @staticmethod
+    def convert_userid(userid: bytes) -> int:
+        return int.from_bytes(userid, byteorder='little', signed=False)
